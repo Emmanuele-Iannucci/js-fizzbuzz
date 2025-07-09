fafb09797d0b15d8111957923d0fc4bd521b0efa
Emmanuele Iannucci <emmanueleiannucci@gmail.com>
2025-07-09 18:12:22 +0200

    add_planning_comments_before_coding

---
+++ main.js
@@ -0,0 +1,8 @@
+// Vogliamo stampare i numeri da 1 a 100
+// Per ogni numero:
+//   - Se è divisibile per 3, stampiamo "Fizz"
+//   - Se è divisibile per 5, stampiamo "Buzz"
+//   - Se è divisibile per sia 3 che 5, stampiamo "FizzBuzz"
+//   - Altrimenti stampiamo il numero stesso
+// Usiamo l'operatore modulo (%) per verificare la divisibilità
+// Ps. per ragionare sui multipli è necessario ragionare sulla divisibilità
