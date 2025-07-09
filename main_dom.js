d828cacd759486c587bade1f5ddfbaa567707d25
Emmanuele Iannucci <emmanueleiannucci@gmail.com>
2025-07-09 20:29:42 +0200

    add_final_print_to_main_dom_js

--- main_dom.js
+++ main_dom.js
@@ -10,3 +10,25 @@ for (let i = 1; i <= 100; i++) {
     // stampa nella pagina e a capo 
     outputDiv.innerHTML += output + "<br>";
 }
+
+// dichiarazione fuori dal ciclo
+// Selezioniamo il div in cui inserire i risultati
+const container = document.getElementById("output");
+
+// Ciclo da 1 a 100
+for (let i = 1; i <= 100; i++) {
+    let output = "";
+
+    if (i % 3 === 0 && i % 5 === 0) {
+        output = "FizzBuzz";
+    } else if (i % 3 === 0) {
+        output = "Fizz";
+    } else if (i % 5 === 0) {
+        output = "Buzz";
+    } else {
+        output = i;
+    }
+
+    // Scrivo il risultato al div, con un <br> per andare a capo
+    outputDiv.innerHTML += output + "<br>";
+}
