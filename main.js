4442976f7af809f7e6fe9660a5faa37b2a7d4bff
Emmanuele Iannucci <emmanueleiannucci@gmail.com>
2025-07-09 20:23:34 +0200

    finished_alternative_main_js

--- main.js
+++ main.js
@@ -12,31 +12,31 @@ for (let i = 1; i <= 100; i++) {
     /* inizializziamo l'output preparando una stringa vuota
     da stampare in modo da poterla utilizzare nei casi specifici che seguiranno
     */
-   let output = ""
+    let output = ""
 
 
-// Se il numero è divisibile per 3, stampiamo "Fizz" (Aggiungo Fizz alla stringa output con +=)
-if (i % 3 === 0 ) {
-    output += "Fizz"
-}
+    // Se il numero è divisibile per 3, stampiamo "Fizz" (Aggiungo Fizz alla stringa output con +=)
+    if (i % 3 === 0) {
+        output += "Fizz"
+    }
 
-// Se il numero è divisibile per 5, stampiamo "Buzz" (Aggiungo Buzz alla stringa output con +=)
-if (i % 5 === 0 ) {
-    output += "Buzz"
-}
+    // Se il numero è divisibile per 5, stampiamo "Buzz" (Aggiungo Buzz alla stringa output con +=)
+    if (i % 5 === 0) {
+        output += "Buzz"
+    }
 
-/* Se il numero è divisibile sia per 3 che per 5, stampiamo "FizzBuzz" 
-(La stampa sarà automatica secondo i passaggi precedenti)*/
+    /* Se il numero è divisibile sia per 3 che per 5, stampiamo "FizzBuzz" 
+    (La stampa sarà automatica secondo i passaggi precedenti)*/
 
-// Se i criteri di divisibilità precedenti non vengono rispettati, la stampa sarà il numero stesso.
+    // Se i criteri di divisibilità precedenti non vengono rispettati, la stampa sarà il numero stesso.
 
-if ( output === "" ) {
-    output = i
-}
+    if (output === "") {
+        output = i
+    }
 
-// stampa dei risultati in console
+    // stampa dei risultati in console
 
-console.log(output);
+    console.log(output);
 
 }
 
@@ -46,6 +46,25 @@ for (let i = 1; i <= 100; i++) {
 
     // Se il numero è divisibile per 3 e per 5, stampiamo "FizzBuzz"
 
-    if (i % 3 === 0 && i % 5 === 0) // && operatore logico - entrambe le condizioni devono essere vere
-    output = "FizzBuzz"; 
+    if (i % 3 === 0 && i % 5 === 0) { // && operatore logico - entrambe le condizioni devono essere vere
+        output = "FizzBuzz";
 
+    } // Se il numero è divisibile solo per 3, assegniamo "Fizz"
+    else if (i % 3 === 0) {
+        output = "Fizz";
+
+    } // Se il numero è divisibile solo per 5, assegniamo "Buzz"
+
+    else if (i % 5 === 0) {
+        output = "Buzz"
+
+    } // Se non si verifica nessuna condizione, assegniamo il numero stesso 
+    else {
+        output = i
+    }
+
+    // Stampa nella console
+
+    console.log(output)
+
+}
