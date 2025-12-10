29398906e1d16a31f097e2a53fc046f3fe4a1e5a
Emmanuele-Iannucci <emmanueleiannucci@gmail.com>
2025-12-10 21:33:50 +0100

    add_if_result_buzz

--- main.js
+++ main.js
@@ -19,7 +19,8 @@ così come lo faremmo "a mano"
 
 for (let i = 1; i <= 100; i++){
     let result = ""
-    if (i % 3 === 0) {result += "Fizz"}
+    if (i % 3 === 0) {result += "Fizz"};
+    if (i % 5 === 0) {result += "Buzz"};
     console.log (result)
 }
 
